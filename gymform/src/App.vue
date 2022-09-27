f6a0ed8ed16a5d2c3b4f6a9a7390b5da46443018
avipalerya <avipaleriya27@gmail.com>
2022-09-27 11:26:21 +0530

    validgymform

---
+++ gymform/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <valid-gym />
+  <!-- <gym-form /> -->
+  <!-- <fetch-data /> -->
+</template>
+
+<script>
+import ValidGym from "./components/Valid-Gym.vue";
+// import FetchData from "./components/FetchData.vue";
+// import GymForm from "./components/GymForm.vue";
+
+export default {
+  name: "App",
+  components: {
+    ValidGym,
+    // FetchData,
+    // GymForm,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ gymform/src/components/FetchData.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Users</h1>
+</template>
+
+<script>
+export default {
+  methods: {
+    async fetchUsers() {
+      try {
+        let res = await fetch("https://jsonplaceholder.typicode.com/users");
+        let data = await res.json();
+        console.log("data", data);
+      } catch (err) {
+        console.log("error", err);
+      }
+    },
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+};
+</script>
+
+<style></style>
